7f7e96a23c5c7ba4da45f44d4627be4b4edd0414
m-kawahigashi <masa.15.30@icloud.com>
2019-06-30 00:15:23 +0900

    投稿画面実装と投稿ができるようにした。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
 
   def index
-    @messages = Message.all
+    @messages = Message.order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    @messages = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.permit(:name, :image, :text)
   end
 
 end
