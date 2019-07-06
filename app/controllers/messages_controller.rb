111346802f77bffca7fc1b8c6243e1a45d2c4f0b
m-kawahigashi <masa.15.30@icloud.com>
2019-07-06 17:57:19 +0900

    編集、削除、commentsテーブル作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,40 @@
 class MessagesController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
-    @messages = Message.page(params[:page]).per(4).order("created_at DESC")
+    @messages = Message.includes(:user).page(params[:page]).per(4).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Message.create(name: message_params[:name], image: message_params[:image], text: message_params[:text], user_id: current_user.id)
+    Message.create(image: message_params[:image], text: message_params[:text], user_id: current_user.id)
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    if message.user_id == current_user.id
+      message.update(message_params)
+    end
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def destroy
+    message = Message.find(params[:id])  #変数messageにmessageテーブルのmessage.idを代入。
+    message.destroy if message.user_id == current_user.id  #もしmessage_idと現在ログインしているユーザーのidがマッチしていたら削除する処理。
   end
 
   private
   def message_params
-    params.permit(:name, :image, :text)
+    params.require(:message).permit(:image, :text)
   end
 
   def move_to_index
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_092617) do
+ActiveRecord::Schema.define(version: 2019_07_06_083046) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "message_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.text "text"
     t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
-    t.index ["name"], name: "index_messages_on_name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
