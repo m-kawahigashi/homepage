56323fd1b913295e0a38563399df8438e0be3ea9
m-kawahigashi <masa.15.30@icloud.com>
2019-08-09 14:24:06 +0900

    image_uploader.rbのコメントアウトを外し、画像のサイズを設定した。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
+
+  process resize_to_fit: [172, 152]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
