e0a2f5a03b4810d2b83de9d33b52f0010d75d1e1
m-kawahigashi <masa.15.30@icloud.com>
2019-09-09 10:03:50 +0900

    jsファイルで、指定したセレクタをクリックするとイベント発火するように記述。

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -2,5 +2,24 @@ $(document).on('turbolinks:load', function(){
   $('.sidebar').hide();  //セレクタ要素を非表示にする記述。
   $('.header__icon').click(function(){  //セレクタ要素をクリックするとイベント発火。
     $('.sidebar').slideToggle();  //イベント発火した時にセレクタ要素を表示する記述。
-  })
-})
+  });
+
+  $('#format').hide();
+  $('.sidebar__box--btn').click(function(){
+    $('#format').fadeIn();
+  });
+  
+
+  $('.back').click(function(){
+    $('#format').fadeOut();
+  });
+
+  $('.search').hide();
+  $('.sidebar__box--search-form').click(function(){
+    $('.search').slideToggle();
+  });
+  // $('.comment').hide();
+  // $('.content__box-btn').click(function(){
+  //   $('.comment').slideToggle();
+  // })
+});
