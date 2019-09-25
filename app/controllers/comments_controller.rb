8c0abbc28dbfd097907b96a3ad7411c8f756ba1e
m-kawahigashi <masa.15.30@icloud.com>
2019-09-25 15:10:49 +0900

    add: コメント削除機能を実装。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
 
   def new
     @message = Message.find(params[:message_id])
@@ -10,11 +11,23 @@ class CommentsController < ApplicationController
     redirect_to "/messages/#{@comment.message.id}"
   end
 
+  def destroy
+    if current_user.id == @comment.user_id  #もしログインしているユーザーidとコメントしたユーザーidが同じであれば
+      @comment.destroy  #コメントを削除する。
+      redirect_to message_path(@comment.message.id)  #コメント削除をしたらmessages/showへリダイレクトする。
+    else
+      redirect_to root_path  #ユーザidとコメントしたユーザーidが等しくなければトップページへリダイレクトさせる。
+    end
+  end
+
+
 private
   def comment_params
     params.require(:comment).permit(:text, :message_id).merge(message_id: params[:message_id], user_id: current_user.id)
   end
 
-end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-# Comment.create(text: comment_params[:text], message_id: comment_params[:message_id], user_id: current_user.id)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
   resources :messages do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create, :destroy]
   end
   resources :users, only: [:show]
 end
